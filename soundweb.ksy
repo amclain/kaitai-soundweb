8e6f9a83ffaaa042d38ac76f70760ef693474414
Alex McLain <alex@alexmclain.com>
2019-01-20 18:00:37 -0800

    Define general message sequence.

--- soundweb.ksy
+++ soundweb.ksy
@@ -6,8 +6,45 @@ seq:
   - id: stx
     contents: [0x02]
   - id: message
+    type: message
     terminator: 0x03
     consume: false
     process: unescape_message
   - id: etx
     contents: [0x03]
+types:
+  message:
+    seq:
+      - id: body
+        type: body
+      - id: checksum
+        type: u1
+        # TODO: checksum valid? ############################################
+  body:
+    seq:
+      - id: command
+        type: u1
+        enum: comamnd
+      - id: node
+        type: u2
+      - id: virtual_device
+        type: u1
+      - id: object
+        # type: u4
+        size: 3
+      - id: state_variable
+        type: u2
+      - id: data
+        type: u4
+        # TODO: CASE ###########################################
+enums:
+  comamnd:
+    0x88: setsv
+    0x89: subscribesv
+    0x8A: unsubscribesv
+    0x8B: venue_preset_recall
+    0x8C: param_preset_recall
+    0x8D: setsvpercent
+    0x8E: subscribesvpercent
+    0x8F: unsubscribesvpercent
+    0x90: bumpsvpercent
--- soundweb.rb
+++ soundweb.rb
@@ -10,6 +10,19 @@ end
 ##
 # BSS Soundweb protocol
 class Soundweb < Kaitai::Struct::Struct
+
+  COMAMND = {
+    136 => :comamnd_setsv,
+    137 => :comamnd_subscribesv,
+    138 => :comamnd_unsubscribesv,
+    139 => :comamnd_venue_preset_recall,
+    140 => :comamnd_param_preset_recall,
+    141 => :comamnd_setsvpercent,
+    142 => :comamnd_subscribesvpercent,
+    143 => :comamnd_unsubscribesvpercent,
+    144 => :comamnd_bumpsvpercent,
+  }
+  I__COMAMND = COMAMND.invert
   def initialize(_io, _parent = nil, _root = self)
     super(_io, _parent, _root)
     _read
@@ -17,14 +30,53 @@ class Soundweb < Kaitai::Struct::Struct
 
   def _read
     @stx = @_io.ensure_fixed_contents([2].pack('C*'))
-    @_raw_message = @_io.read_bytes_term(3, false, false, true)
+    @_raw__raw_message = @_io.read_bytes_term(3, false, false, true)
     _process = UnescapeMessage.new()
-    @message = _process.decode(@_raw_message)
+    @_raw_message = _process.decode(@_raw__raw_message)
+    io = Kaitai::Struct::Stream.new(@_raw_message)
+    @message = Message.new(io, self, @_root)
     @etx = @_io.ensure_fixed_contents([3].pack('C*'))
     self
+  end
+  class Message < Kaitai::Struct::Struct
+    def initialize(_io, _parent = nil, _root = self)
+      super(_io, _parent, _root)
+      _read
+    end
+
+    def _read
+      @body = Body.new(@_io, self, @_root)
+      @checksum = @_io.read_u1
+      self
+    end
+    attr_reader :body
+    attr_reader :checksum
+  end
+  class Body < Kaitai::Struct::Struct
+    def initialize(_io, _parent = nil, _root = self)
+      super(_io, _parent, _root)
+      _read
+    end
+
+    def _read
+      @command = Kaitai::Struct::Stream::resolve_enum(Soundweb::COMAMND, @_io.read_u1)
+      @node = @_io.read_u2be
+      @virtual_device = @_io.read_u1
+      @object = @_io.read_bytes(3)
+      @state_variable = @_io.read_u2be
+      @data = @_io.read_u4be
+      self
+    end
+    attr_reader :command
+    attr_reader :node
+    attr_reader :virtual_device
+    attr_reader :object
+    attr_reader :state_variable
+    attr_reader :data
   end
   attr_reader :stx
   attr_reader :message
   attr_reader :etx
   attr_reader :_raw_message
+  attr_reader :_raw__raw_message
 end
